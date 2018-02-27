b22485576f9cdb51b827caf6f5199ad2858b7df2
Bogdan Kazaryan <bogdankazaryan@yandex.ru>
2018-02-27 23:46:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function getZerosCount(number) {
   let counter = 0;
   for (let i = 5; Math.floor(number / i) >= 1; i *= 5) {
-    counter += Math.floor(number/i);
+    counter += Math.floor(number / i);
   }
   return counter;
 }
